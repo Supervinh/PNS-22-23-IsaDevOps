f1eca9145335dbba9fa681b9764102adc0b8d7c0
TomBevan <bevantom@protonmail.com>
2023-02-12 16:15:58 +0100

    Début tests #2

---
+++ backend/src/test/java/components/CustomerRegistryTest.java
@@ -0,0 +1,66 @@
+package components;
+
+import mfc.interfaces.explorer.CustomerFinder;
+import mfc.interfaces.modifier.CustomerRegistration;
+import mfc.repositories.CustomerRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+@SpringBootTest
+class CustomerRegistryTest {
+
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    @Autowired
+    private CustomerRegistration customerRegistration;
+
+    @Autowired
+    private CustomerFinder customerFinder;
+
+    @Test
+    void findCustomerByMail() {
+    }
+
+    @Test
+    void findCustomerById() {
+    }
+
+    @Test
+    void editBalance() {
+    }
+
+    @Test
+    void editFidelityPoints() {
+    }
+
+    @Test
+    void recordMatriculation() {
+    }
+
+    @Test
+    void recordCreditCard() {
+    }
+
+    @Test
+    void recordNewFavoriteStore() {
+    }
+
+    @Test
+    void removeFavoriteStore() {
+    }
+
+    @Test
+    void recordNewFavoriteStores() {
+    }
+
+    @Test
+    void removeAllFavoriteStores() {
+    }
+
+    @Test
+    void register() {
+    }
+}
