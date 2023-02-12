48e2693402da3dc70245805d903007652d2a94bd
TomBevan <bevantom@protonmail.com>
2023-02-12 16:37:22 +0100

    Ajout des tests pour CustomerRegistry #2

--- backend/src/test/java/mfc/components/CustomerRegistryTest.java
+++ backend/src/test/java/mfc/components/CustomerRegistryTest.java
@@ -1,11 +1,18 @@
 package mfc.components;
 
+import mfc.POJO.Customer;
+import mfc.interfaces.exceptions.AlreadyExistingAccountException;
 import mfc.interfaces.explorer.CustomerFinder;
 import mfc.interfaces.modifier.CustomerRegistration;
 import mfc.repositories.CustomerRepository;
+import org.junit.Before;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
@@ -20,47 +27,38 @@ class CustomerRegistryTest {
     @Autowired
     private CustomerFinder customerFinder;
 
-    @Test
-    void findCustomerByMail() {
+    private String mail = "Mark@pns.fr";
+    private String name = "Mark";
+    private String password = "password";
+
+
+    @BeforeEach
+    void setUp() {
+        customerRepository.deleteAll();
     }
 
     @Test
-    void findCustomerById() {
+    public void unknownCustomer() {
+        assertFalse(customerRepository.findByMail(mail).isPresent());
     }
 
     @Test
-    void editBalance() {
+    public void registerCustomer() throws Exception {
+        Customer returned = customerRegistration.register(mail, name, password);
+        Optional<Customer> customer = customerFinder.findCustomerByMail(mail);
+        assertTrue(customer.isPresent());
+        Customer mark = customer.get();
+        assertEquals(mark, returned);
+        assertEquals(mark, customerFinder.findCustomerById(returned.getId()).get());
+        assertEquals(name, mark.getName());
+        assertEquals(mail, mark.getMail());
     }
 
     @Test
-    void editFidelityPoints() {
-    }
-
-    @Test
-    void recordMatriculation() {
-    }
-
-    @Test
-    void recordCreditCard() {
-    }
-
-    @Test
-    void recordNewFavoriteStore() {
-    }
-
-    @Test
-    void removeFavoriteStore() {
-    }
-
-    @Test
-    void recordNewFavoriteStores() {
-    }
-
-    @Test
-    void removeAllFavoriteStores() {
-    }
-
-    @Test
-    void register() {
+    public void cannotRegisterTwice() throws Exception {
+        customerRegistration.register(mail, name, password);
+        Assertions.assertThrows(AlreadyExistingAccountException.class, () -> {
+            customerRegistration.register(mail, name, password);
+        });
     }
 }
