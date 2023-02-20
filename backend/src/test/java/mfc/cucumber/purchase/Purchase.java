76300c198f6049ec628bdb62167830bf2f8f52a6
TomBevan <bevantom@protonmail.com>
2023-02-20 11:07:04 +0100

    Uncomment purchase's cucmberTest #2

--- backend/src/test/java/mfc/cucumber/purchase/Purchase.java
+++ backend/src/test/java/mfc/cucumber/purchase/Purchase.java
@@ -1,5 +1,10 @@
 package mfc.cucumber.purchase;
 
+import io.cucumber.java.en.*;
+import mfc.POJO.Customer;
+import mfc.exceptions.AlreadyExistingAccountException;
+import mfc.exceptions.AlreadyExistingStoreException;
+import mfc.exceptions.InsufficientBalanceException;
 import mfc.interfaces.TransactionProcessor;
 import mfc.interfaces.explorer.CustomerFinder;
 import mfc.interfaces.explorer.StoreFinder;
@@ -34,59 +39,58 @@ public class Purchase {
     @Autowired
     private StoreOwnerRepository storeOwnerRepository;
 
+    @Given("a customer named {string} with {string} as mail address and {string} as password")
+    public void aCustomerNamedWithAsMailAddressAndAsPassword(String name, String mail, String password) throws AlreadyExistingAccountException {
+        customerRepository.deleteAll();
+        customerRegistration.register(name, mail,  password);
+    }
 
-//    @Given("a customer named {string} with {string} as mail address and {string} as password")
-//    public void aCustomerNamedWithAsMailAddressAndAsPassword(String name, String mail, String password) throws AlreadyExistingAccountException {
-//        customerRepository.deleteAll();
-//        customerRegistration.register(mail, name, password);
-//    }
+    @Given("a store owner named {string} with {string} as mail address and {string} as password")
+    public void aStoreOwnerNamedWithAsMailAddressAndAsPassword(String name, String mail, String password) throws AlreadyExistingAccountException {
+        storeOwnerRepository.deleteAll();
+        storeOwnerRegistration.registerStoreOwner(name, mail, password);
+    }
 
-//    @Given("a store owner named {string} with {string} as mail address and {string} as password")
-//    public void aStoreOwnerNamedWithAsMailAddressAndAsPassword(String name, String mail, String password) throws AlreadyExistingAccountException {
-//        storeOwnerRepository.deleteAll();
-//        storeOwnerRegistration.registerStoreOwner(name, mail, password);
-//    }
-//
-//    @Given("a store named {string}, owned by {string}, with opening hours from {int}:{int} to {int}:{int}")
-//    public void aStoreNamedOwnedByWithOpeningHoursFromTo(String storeName, String ownerMail, int openingHour, int openingMinute, int closingHour, int closingMinute) throws AlreadyExistingStoreException, AlreadyExistingAccountException {
-//        storeRepository.deleteAll();
-//        Map<LocalTime, LocalTime> openingHours = Map.of(LocalTime.of(openingHour, openingMinute), LocalTime.of(closingHour, closingMinute));
-//        storeRegistration.register(openingHours, storeOwnerRepository.findByMail(ownerMail).get(), storeName);
-//    }
-//
-//    @Given("{string} has {int} points")
-//    public void hasPoints(String name, int points) {
-//        Customer customer = customerFinder.findCustomerByName(name).get();
-//        customer.setFidelityPoints(points);
-//    }
-//
-//
-//    @When("{string} makes a purchase of {int} euros at the store {string}")
-//    public void makesAPurchaseOfEuros(String name, int cost, String storeName) {
-//        Customer customer = customerFinder.findCustomerByName(name).get();
-//        transactionProcessor.purchase(customer, cost, storeFinder.findStoreByName(storeName).get());
-//    }
-//
-//
-//    @Then("{string} should have {int} points")
-//    public void shouldHavePoints(String name, int points) {
-//        Customer customer = customerFinder.findCustomerByName(name).get();
-//        assert customer.getFidelityPoints() == points;
-//    }
-//
-//    @And("{string} has a fidelity card with a balance of {double} euros")
-//    public void hasAFidelityCardWithABalanceOfEuros(String name, double balance) {
-//        Customer customer = customerFinder.findCustomerByName(name).get();
-//        customer.setBalance(balance);
-//    }
-//
-//    @When("{string} makes a purchase of {int} euros with the fidelity card at the store {string}")
-//    public void makesAPurchaseOfEurosWithTheFidelityCard(String name, int cost, String storeName) {
-//        Customer customer = customerFinder.findCustomerByName(name).get();
-//        try {
-//            transactionProcessor.purchaseFidelityCardBalance(customer, cost, storeFinder.findStoreByName(storeName).get());
-//        } catch (InsufficientBalanceException e) {
-//            throw new RuntimeException(e);
-//        }
-//    }
+    @Given("a store named {string}, owned by {string}, with opening hours from {int}:{int} to {int}:{int}")
+    public void aStoreNamedOwnedByWithOpeningHoursFromTo(String storeName, String ownerMail, int openingHour, int openingMinute, int closingHour, int closingMinute) throws AlreadyExistingStoreException, AlreadyExistingAccountException {
+        storeRepository.deleteAll();
+        //Map<LocalTime, LocalTime> openingHours = Map.of(LocalTime.of(openingHour, openingMinute), LocalTime.of(closingHour, closingMinute));TODO: change this when opening hours are implemented
+        storeRegistration.register(storeOwnerRepository.findByMail(ownerMail).get(), storeName);
+    }
+
+    @Given("{string} has {int} points")
+    public void hasPoints(String name, int points) {
+        Customer customer = customerFinder.findCustomerByName(name).get();
+        customer.setFidelityPoints(points);
+    }
+
+
+    @When("{string} makes a purchase of {int} euros at the store {string}")
+    public void makesAPurchaseOfEuros(String name, int cost, String storeName) {
+        Customer customer = customerFinder.findCustomerByName(name).get();
+        transactionProcessor.purchase(customer, cost, storeFinder.findStoreByName(storeName).get());
+    }
+
+
+    @Then("{string} should have {int} points")
+    public void shouldHavePoints(String name, int points) {
+        Customer customer = customerFinder.findCustomerByName(name).get();
+        assert customer.getFidelityPoints() == points;
+    }
+
+    @And("{string} has a fidelity card with a balance of {double} euros")
+    public void hasAFidelityCardWithABalanceOfEuros(String name, double balance) {
+        Customer customer = customerFinder.findCustomerByName(name).get();
+        customer.setBalance(balance);
+    }
+
+    @When("{string} makes a purchase of {int} euros with the fidelity card at the store {string}")
+    public void makesAPurchaseOfEurosWithTheFidelityCard(String name, int cost, String storeName) {
+        Customer customer = customerFinder.findCustomerByName(name).get();
+        try {
+            transactionProcessor.purchaseFidelityCardBalance(customer, cost, storeFinder.findStoreByName(storeName).get());
+        } catch (InsufficientBalanceException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
