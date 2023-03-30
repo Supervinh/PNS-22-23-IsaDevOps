509f98cd6a564fa0daf53ab159498e8203f05f65
TomBevan <bevantom@protonmail.com>
2023-03-30 10:12:13 +0200

    Auto stash before merge of "feature/TUTI" and "origin/feature/TUTI"

--- cli/build.sh
+++ cli/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ -z "$1" ]; then
-  echo "Compiling the TCF Spring CLI within a multi-stage docker build"
+  echo "Compiling the MFC Spring CLI within a multi-stage docker build"
   docker build --build-arg JAR_FILE=cli-*.jar -t jeannestheo/mfc-spring-cli .
 else
   docker build --build-arg JAR_FILE=cli.jar -f DeployementDockerfile -t jeannestheo/mfc-spring-cli .
