903c88fc2be74c13e7ddaa0fd984e8cf79b90b9f
TomBevan <bevantom@protonmail.com>
2023-04-03 17:20:40 +0200

    Fix payOff test #22

--- backend/src/test/java/mfc/cucumber/usePoints/PayOff.java
+++ backend/src/test/java/mfc/cucumber/usePoints/PayOff.java
@@ -7,9 +7,11 @@ import mfc.entities.Payoff;
 import mfc.entities.Store;
 import mfc.exceptions.*;
 import mfc.interfaces.PayOffProcessor;
+import mfc.interfaces.explorer.CatalogExplorer;
 import mfc.interfaces.explorer.CustomerFinder;
 import mfc.interfaces.explorer.PayOffPurchaseFinder;
 import mfc.interfaces.explorer.StoreFinder;
+import mfc.interfaces.modifier.CatalogModifier;
 import mfc.repositories.PayoffRepository;
 import mfc.repositories.StoreRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,10 +21,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 public class PayOff {
 
     @Autowired
-    private PayoffRepository payoffRepository;
+    private CatalogModifier catalogModifier;
 
     @Autowired
-    private StoreRepository storeRepository;
+    private CatalogExplorer catalogExplorer;
 
     @Autowired
     private StoreFinder storeFinder;
@@ -35,15 +37,14 @@ public class PayOff {
 
 
     @Given("a payOff named {string}, which costs {int} euros, which can be exchanged for {int} points and is available at {string}")
-    public void aPayOffNamedWhichCostsEurosWhichCanBeExchangedForPointsAndIsAvailableAt(String name, int cost, int pointCost, String store) {
+    public void aPayOffNamedWhichCostsEurosWhichCanBeExchangedForPointsAndIsAvailableAt(String name, int cost, int pointCost, String store) throws NegativePointCostException, NegativeCostException, AlreadyExistingPayoffException {
         Store store1 = storeFinder.findStoreByName(store).get();
-        Payoff payoff = new Payoff(name, cost, pointCost, store1);
-        payoffRepository.save(payoff);
+        catalogModifier.addPayOff(name, cost, pointCost, store1, false);
     }
 
     @When("{string} use points to buy {string} at {string}")
-    public void usePointsToBuy(String name, String payoffName, String storeName) throws NegativePointCostException, NoMatriculationException, VFPExpiredException, ParkingException, CustomerNotFoundException {
-        Payoff payoff = payoffRepository.findPayoffByNameAndStore_Name(payoffName, storeName).get();
+    public void usePointsToBuy(String name, String payoffName, String storeName) throws NegativePointCostException, NoMatriculationException, VFPExpiredException, ParkingException, CustomerNotFoundException, InsufficientBalanceException, PayoffNotFoundException, NoPreviousPurchaseException {
+        Payoff payoff = catalogExplorer.findPayoff(payoffName, storeName).get();
         Customer customer = customerFinder.findCustomerByName(name).get();
         payOffProcessor.claimPayoff(customer, payoff);
     }
